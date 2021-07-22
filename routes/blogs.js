5a100fc853fc2ce1a4f4f6100e309bef052acfdb
Bryan Leyva <bryan.leyva@spaceforce.mil>
2021-07-23 00:12:16 +0200

    start update

--- routes/blogs.js
+++ routes/blogs.js
@@ -3,12 +3,12 @@ var router = express.Router();
 const knex = require('knex')(require('../knexfile.js')['development']);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   knex
     .select('*')
     .from('blogs')
     .orderBy('id', 'desc')
-    .then(data => res.status(200).json(data).end())
+    .then(data => res.status(200).json(data))
     .catch(err =>
       res.status(404).json({
         message:
@@ -16,5 +16,35 @@ router.get('/', function(req, res, next) {
       })
     );
 });
+
+router.post('/', function(req, res) {
+  knex('blogs')
+    .insert({
+      author: req.body.author,
+      title: req.body.title,
+      text: req.body.text,
+      image: req.body.image
+    })
+    .then(response => res.status(200).json({message: 'Success'}))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'Failed to post blog'
+      })
+    );
+})
+
+router.delete('/', function(req, res) {
+  knex('blogs')
+    .del()
+    .where('id', req.body.id)
+    .then(response => res.status(200).json({message: 'Success'}))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'Failed to post blog'
+      })
+    );
+})
 
 module.exports = router;
